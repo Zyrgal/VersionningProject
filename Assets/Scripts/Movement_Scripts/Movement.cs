9e0817af7b026d0c5ef7f2359edf595303db73d7
Karim <karimal-homsi@hotmail.com>
2023-01-20 11:12:37 +0100

    [ADD]MovementFeatur_Jump
    I added the jump action so the player can jump over hurdles

--- Assets/Scripts/Movement_Scripts/Movement.cs
+++ Assets/Scripts/Movement_Scripts/Movement.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float MovementSpeed = 1;
+    public float JumpForce = 1;
+
+    private Rigidbody2D _rigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -17,5 +20,10 @@ public class Movement : MonoBehaviour
     {
         var movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) <0.001f)
+        {
+            _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
     }
 }
